cad7ba1020e11d16c695f1dc0a8aba898becff30
Vesko V <payvesipal@gmail.com>
2026-02-25 09:01:40 +0200

    fix ripgrep globs

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -2,10 +2,25 @@ local telescope = require "telescope"
 local opts = require "nvchad.configs.telescope"
 
 opts.defaults.file_ignore_patterns = opts.defaults.file_ignore_patterns or {}
+
 vim.list_extend(opts.defaults.file_ignore_patterns, {
   "node_modules/",
   "%.git/",
 })
+
+-- ignore for find_files / general telescope filtering
+opts.defaults.file_ignore_patterns = opts.defaults.file_ignore_patterns or {}
+
+vim.list_extend(opts.defaults.file_ignore_patterns, {
+  "node_modules/",
+  "%.git/",
+})
+
+-- ignore for live_grep (ripgrep args)
+local rg_args = opts.defaults.vimgrep_arguments or require("telescope.config").values.vimgrep_arguments
+
+-- make a copy so we never mutate Telescope's shared default table
+opts.defaults.vimgrep_arguments = vim.deepcopy(rg_args)
 
 vim.list_extend(opts.defaults.vimgrep_arguments, {
   "--glob",
